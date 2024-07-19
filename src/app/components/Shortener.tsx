4698548f9e45957c5046933c11cebe26d84ffef0
Rajendra Kumar <rajendrakbanjara@gmail.com>
2024-07-19 22:46:23 +0530

    create shortener link interface

--- src/app/components/Shortener.tsx
+++ src/app/components/Shortener.tsx
@@ -1,23 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import shortener from "/public/images/bg-shorten-mobile.svg";
 export default function Shortener() {
-  // https://ulvis.net/API/write/get?url=
-
   const [text, setText] = useState("");
-  const [link, setLink] = useState([]);
-  const handleSubmit = (e:any)=>{
-    e.preventDefault()
-    if(!text){
+  const [linkData, setLinkData] = useState(null);
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (!text) {
       alert("input is empty");
-    }else{
-      const shortenLink = async () =>{
+    } else {
+      setSubmit(true);
+    }
+  };
+
+  useEffect(() => {
+    if (submit) {
+      const shortenLink = async () => {
         const res = await fetch(`https://ulvis.net/API/write/get?url=${text}`);
         const data = await res.json();
-        setLink(data.result);
-      }
+        setLinkData(data.data);
+        setText(""); // empty the input field
+      };
       shortenLink();
+      setSubmit(false);
     }
-  }
+  }, [submit, text]);
+  console.log("here the result");
   return (
     <>
       <section className="shortener max-width relative">
@@ -33,7 +42,7 @@ export default function Shortener() {
               className="w-full py-2 px-5 rounded-lg mb-2 md:mb-0 md:w-2/3"
               type="url"
               placeholder="Shorten a link here"
-              onChange={(e)=>setText(e.target.value)}
+              onChange={(e) => setText(e.target.value)}
             />
             <button
               onClick={handleSubmit}
@@ -43,10 +52,34 @@ export default function Shortener() {
             >
               Shorten it
             </button>
-            <h1>{link}</h1>
           </div>
         </form>
+        {/* display shorted link */}
+        <div
+          className="bg-white flex flex-col justify-center items-center text-center md:flex-row 
+        md:justify-between p-3 mt-3 rounded-lg shadow"
+        >
+          <article>
+            <h6 className="mb-3 md:mb-0">original link</h6>
+          </article>
+          <article >
+            <ul className="md:flex md:items-center">
+              <li className="md:mr-5">
+                <button className="text-cyan-500">link is here</button>
+              </li>
+              <li>
+                <button
+                  className="text-white px-3 py-2 text-sm rounded-lg"
+                  style={{ backgroundColor: "hsl(180, 66%, 49%)" }}
+                >
+                  copy
+                </button>
+              </li>
+            </ul>
+          </article>
+        </div>
       </section>
     </>
   );
 }
+// {linkData && <h1>{linkData.url}</h1>}
